ca30c7684156975cf6f2298d08b568737bf7f1a6
nadyaaudrey <audrey.hime@yahoo.com>
2019-02-28 17:49:41 -0800

    Push current

---
+++ js/portfolio2.js
@@ -0,0 +1,171 @@
+<!--hide
+//Image 1 - Get the modal
+var mod = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    mod.style.display = "block";
+    modImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("exit")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  mod.style.display = "none";
+}
+
+
+//Image 2
+var mod2 = document.getElementById('myModal2');
+var img2 = document.getElementById('myImg2');
+var modImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption2");
+img2.onclick = function(){
+    mod2.style.display = "block";
+    modImg2.src = this.src;
+    captionText2.innerHTML = this.alt;
+}
+
+var span2 = document.getElementsByClassName("exit2")[0];
+
+span2.onclick = function() { 
+  mod2.style.display = "none";
+}
+
+//Image 3
+var mod3 = document.getElementById('myModal3');
+var img3 = document.getElementById('myImg3');
+var modImg3 = document.getElementById("img03");
+var captionText3 = document.getElementById("caption3");
+img3.onclick = function(){
+    mod3.style.display = "block";
+    modImg3.src = this.src;
+    captionText3.innerHTML = this.alt;
+}
+
+var span3 = document.getElementsByClassName("exit3")[0];
+
+span3.onclick = function() { 
+  mod3.style.display = "none";
+}
+
+//Image 4 - Get the modal
+var mod4 = document.getElementById('myModal4');
+var img4 = document.getElementById('myImg4');
+var modImg4 = document.getElementById("img04");
+var captionText4 = document.getElementById("caption4");
+img4.onclick = function(){
+    mod4.style.display = "block";
+    modImg4.src = this.src;
+    captionText4.innerHTML = this.alt;
+}
+
+var span4 = document.getElementsByClassName("exit4")[0];
+
+span4.onclick = function() { 
+  mod4.style.display = "none";
+}
+
+//Image 5 - Get the modal
+var mod5 = document.getElementById('myModal5');
+var img5 = document.getElementById('myImg5');
+var modImg5 = document.getElementById("img05");
+var captionText5 = document.getElementById("caption5");
+img5.onclick = function(){
+    mod5.style.display = "block";
+    modImg5.src = this.src;
+    captionText5.innerHTML = this.alt;
+}
+
+var span5 = document.getElementsByClassName("exit5")[0];
+
+span5.onclick = function() { 
+  mod5.style.display = "none";
+}
+//Image 6 - Get the modal
+var mod6 = document.getElementById('myModal6');
+var img6 = document.getElementById('myImg6');
+var modImg6 = document.getElementById("img06");
+var captionText6 = document.getElementById("caption6");
+img6.onclick = function(){
+    mod6.style.display = "block";
+    modImg6.src = this.src;
+    captionText6.innerHTML = this.alt;
+}
+
+var span6 = document.getElementsByClassName("exit6")[0];
+
+span6.onclick = function() { 
+  mod6.style.display = "none";
+}
+//Image 7 - Get the modal
+var mod7 = document.getElementById('myModal7');
+var img7 = document.getElementById('myImg7');
+var modImg7 = document.getElementById("img07");
+var captionText7 = document.getElementById("caption7");
+img7.onclick = function(){
+    mod7.style.display = "block";
+    modImg7.src = this.src;
+    captionText7.innerHTML = this.alt;
+}
+
+var span7 = document.getElementsByClassName("exit7")[0];
+
+span7.onclick = function() { 
+  mod7.style.display = "none";
+}
+//Image 8 - Get the modal
+var mod8 = document.getElementById('myModal8');
+var img8 = document.getElementById('myImg8');
+var modImg8 = document.getElementById("img08");
+var captionText8 = document.getElementById("caption8");
+img8.onclick = function(){
+    mod8.style.display = "block";
+    modImg8.src = this.src;
+    captionText8.innerHTML = this.alt;
+}
+
+var span8 = document.getElementsByClassName("exit8")[0];
+
+span8.onclick = function() { 
+  mod8.style.display = "none";
+}
+//Image 9 - Get the modal
+var mod9 = document.getElementById('myModal9');
+var img9 = document.getElementById('myImg9');
+var modImg9 = document.getElementById("img09");
+var captionText9 = document.getElementById("caption9");
+img9.onclick = function(){
+    mod9.style.display = "block";
+    modImg9.src = this.src;
+    captionText9.innerHTML = this.alt;
+}
+
+var span9 = document.getElementsByClassName("exit9")[0];
+
+span9.onclick = function() { 
+  mod9.style.display = "none";
+}
+//Image 10 - Get the modal
+var mod10 = document.getElementById('myModal10');
+var img10 = document.getElementById('myImg10');
+var modImg10 = document.getElementById("img10");
+var captionText10 = document.getElementById("caption10");
+img10.onclick = function(){
+    mod10.style.display = "block";
+    modImg10.src = this.src;
+    captionText10.innerHTML = this.alt;
+}
+
+var span10 = document.getElementsByClassName("exit10")[0];
+
+span10.onclick = function() { 
+  mod10.style.display = "none";
+}
+-->
